147a738f22696adae051d0e13a27b8b2a7d9bd8c
torresomar <torres.omar.eduardo@gmail.com>
2015-10-10 02:00:35 -0500

    Add support for ES6 and browserify
    Include browserify for module requirements and use babelify in order to
    transpile ES6 code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 // Include gulp
-var gulp            = require('gulp');
-
+var gulp = require('gulp');
 // Include Our Plugins
-var $               = require('gulp-load-plugins')({ lazy: true });
+var $ = require('gulp-load-plugins')({
+    lazy: true
+});
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
 
 var paths = {
     html: ['app/*.html'],
     scripts: [
         'app/scripts/*.js',
-        'lib/**/*.js'
+        'lib/**/*.js',
+        'gulpfile.js'
     ],
     styles: [
         'app/styles/**/*.scss'
     ]
 };
-
 
 gulp.task('lint', function() {
     console.log('Linting the js code');
@@ -23,6 +27,18 @@ gulp.task('lint', function() {
     .pipe($.jscs())
     .pipe($.jscs.reporter())
     .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish' , { verbose: true }))
+    .pipe($.jshint.reporter('jshint-stylish' , {verbose: true}))
     .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('scripts', function() {
+    browserify({
+        entries: 'lib/sample.jsx',
+        extensions: ['.jsx'],
+        debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist/scripts/'));
 });
