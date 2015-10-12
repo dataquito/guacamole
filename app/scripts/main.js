5fba6b9c5b31fbd433514b197b94d88410435020
torresomar <torres.omar.eduardo@gmail.com>
2015-10-12 01:20:35 -0500

    Add prop functions to extend component.
    Include click, mouseover, mouseout function for feature elements.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,25 @@
 var React       = require('react');
 var GoogleMap   = require('../../lib/index').GoogleMap;
 
-React.render(<GoogleMap/>, document.getElementById('guacamole-main'));
+var mouseOverPolygon = function(map, event) {
+    map.data.revertStyle();
+    map.data.overrideStyle(event.feature, {
+        strokeWeight: 1,
+        fillColor: '#ddd'
+    });
+}
+
+var mouseOutPolygon = function(map, event) {
+    map.data.revertStyle();
+}
+
+var mouseClickPolygon = function(map, event) {
+    console.log('click');
+}
+
+React.render(<GoogleMap 
+    mouseOver={mouseOverPolygon} 
+    mouseOut={mouseOutPolygon}
+    click={mouseClickPolygon}/>, 
+        document.getElementById('guacamole-main'));
+
--- lib/GoogleMap.jsx
+++ lib/GoogleMap.jsx
@@ -14,7 +14,42 @@ var GoogleMap = React.createClass({
             },
             latitude: 19.426875,
             longitude:  -99.134564,
-            zooom: 8
+            zoom: 5,
+            colors: ['#ffffcc','#c7e9b4','#7fcdbb','#41b6c4','#2c7fb8','#253494'],
+            entities: {
+                "01": 19488135081.709801,
+                "02": 62162383771.269501,
+                "03": 8677489107.6500092,
+                "04": 5386653998.8299999,
+                "05": 60250348211.538696,
+                "06": 11925029956.2701,
+                "07": 10902890182.110001,
+                "08": 62017203696.9702,
+                "09": 65164893728.738297,
+                "10": 16134737778.579901,
+                "11": 59084560926.729401,
+                "12": 7484948287.89991,
+                "13": 26084459366.680099,
+                "14": 90762140351.971603,
+                "15": 90078053418.343506,
+                "16": 23557588087.300301,
+                "17": 10350225109.459801,
+                "18": 10535000038.459999,
+                "19": 133036205046.681,
+                "20": 5256021129.7999897,
+                "21": 30994818005.610401,
+                "22": 28267209445.6702,
+                "23": 36394428458.370399,
+                "24": 25377438561.879902,
+                "25": 37796567391.579903,
+                "26": 52019113722.980904,
+                "27": 12574651445.92,
+                "28": 68535284355.1698,
+                "29": 6770710677.97999,
+                "30": 45199672149.341003,
+                "31": 27430206259.249901,
+                "32": 7001951665.3199501
+            }
         };
     },
     getInitialState: function() {
@@ -31,7 +66,7 @@ var GoogleMap = React.createClass({
             center: latlng,
             styles: mapStyle,
             disableDefaultUI: true,
-            zoom: 8
+            zoom: this.props.zoom
         };
         this._map = new google.maps.Map(React.findDOMNode(this), mapOptions);
         this._loadPolygons();
@@ -75,23 +110,33 @@ var GoogleMap = React.createClass({
             fillOpacity: 0.2
         };
     },
+    _getRandomColor: function() {
+        var colors = this.props.colors;
+        return _.sample(colors);
+    },
     _addPolygonListeners: function() {
         var map = this._map;
         google.maps.event.addListener(map.data, 'mouseover', this._mouseOverPolygon);
         google.maps.event.addListener(map.data, 'mouseout', this._mouseOutPolygon);
-        // google.maps.event.addListener(map.data, 'mouseout', function() {
-        //     map.data.revertStyle();
-        //     _this.setState({place: null, placeInfo: null});
-        // });
+        google.maps.event.addListener(map.data, 'click', this._mouseClickPolygon);
     },
     _mouseOverPolygon: function(event) {
         var map = this._map;
-        map.data.revertStyle();
-        map.data.overrideStyle(event.feature, {strokeWeight: 1});
+        if(typeof this.props.mouseOver === 'function'){
+            this.props.mouseOver(map, event);
+        }
     },
     _mouseOutPolygon: function() {
         var map = this._map;
-        map.data.revertStyle();
+        if(typeof this.props.mouseOut === 'function'){
+            this.props.mouseOut(map);
+        }
+    },
+    _mouseClickPolygon: function(event) {
+        var map = this._map;
+        if(typeof this.props.click === 'function'){
+            this.props.click(map);
+        }
     },
     render: function () {
         var style = this.props.containerStyles;
