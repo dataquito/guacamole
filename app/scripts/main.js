c1e65918ded6b8e0ecacc1351e48ad60a328beab
torresomar <torres.omar.eduardo@gmail.com>
2015-10-13 03:36:15 -0500

    Include layer radio buttons for user election.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,9 @@ var Guacamole = React.createClass({
     getInitialState: function() {
         return {
             view: 'states',
-            name: 'Hover over feature'
+            name: 'Hover over feature',
+            data: null,
+            quantiles: null
         }
     },
     _switchView: function() {
@@ -36,17 +38,59 @@ var Guacamole = React.createClass({
             name: name
         });
     },
+    _requestJSON: function(jsonName) {
+        var endpoint = 'https://s3-us-west-2.amazonaws.com/dataquito-open-data/mexico/inegi/';
+        console.log(jsonName);
+    },
     render: function() {
         return (
             <div style={this.props.style}>
-                <SideBar switchView={this._switchView} view={this.state.view} name={this.state.name}/>
-                <GoogleMap view={this.state.view} mouseOver={mouseOverPolygon} mouseOut={mouseOutPolygon} click={this._click}/>
+                <SideBar switchView={this._switchView} view={this.state.view} name={this.state.name} layerChange={this._requestJSON}/>
+                <GoogleMap view={this.state.view}  mouseOver={mouseOverPolygon} mouseOut={mouseOutPolygon} click={this._click}/>
             </div>
         );
     }
 });
 
 var SideBar = React.createClass({
+    getDefaultProps: function() {
+        return {
+            inegi: [
+                {name: 'Poblacion', json: 'population.json'},
+                {name: 'Pobreza Percentage', json: 'poverty_percentage.json'},
+                {name: 'Pobreza Population', json: 'poverty_population.json'},
+                {name: 'Pobreza Extrema Percentage', json: 'extreme_poverty_percentage.json'},
+                {name: 'Pobreza Extrema Population', json: 'extreme_poverty_population.json'},
+                {name: 'Pobreza Moderada Percentage', json: 'moderate_poverty_percentage.json'},
+                {name: 'Pobreza Moderada Population', json: 'moderate_poverty_population.json'},
+                // {name: 'Vulnerables por carencia social', json: ''},
+                // {name: 'Vulnerables por ingreso'},
+                // {name: 'Rezago educativo'},
+                // {name: 'Carencia por acceso a los servicios de salud'},
+                // {name: 'Carencia por acceso a la seguridad social'},
+                // {name: 'Carencia por calidad y espacios de la vivienda'},
+                // {name: 'Carencia por acceso a los servicios básicos en la vivienda'},
+                // {name: 'Carencia por acceso a la alimentación'},
+                // {name: 'Población con ingreso inferior a la línea de bienestar'},
+                // {name: 'Población con ingreso inferior a la línea de bienestar mínimo'},
+                // {name: 'Coeficiente de Gini'},
+                // {name: 'Razón del ingreso entre la población pobre extrema y la población no pobre y no vulnerable'}
+            ]
+        };
+    },
+    getInitialState: function() {
+        return {
+            layer: 'Poblacion'
+        }
+    },
+    _onLayerChange: function(e, i) {
+        console.log(e.currentTarget, e);
+        this.setState({
+            layer: e.currentTarget.value
+        },function() {
+            this.props.layerChange(e);
+        });
+    },
     render: function() {
         var style = {
             position: 'absolute',
@@ -58,7 +102,20 @@ var SideBar = React.createClass({
             zIndex: 9999,
             borderTop: '2px solid #e74c3c'
         }
-
+        var radioButtons = this.props.inegi.map(function(layer) {
+            return (
+                <div className='radio'>
+                    <label>
+                        <input type='radio'
+                            name='layers'
+                            value={layer.name} 
+                            checked={this.state.layer === layer.name} 
+                            onChange={this._onLayerChange}/>
+                        {layer.name}
+                    </label>
+                </div>
+            );
+        }, this);
         return (
             <div style={style}>
                 <div className='col-sm-12'>
@@ -70,7 +127,7 @@ var SideBar = React.createClass({
                         {this.props.name}
                     </h4>
                     <p style={{color: '#fff'}}>
-                        Duo ei choro labores. Erat dolorem convenire ea sed. Qui wisi omnesque ad, per cu voluptatum intellegam. Brute movet mediocrem ad mea, vis simul partiendo adipiscing te, assueverit complectitur mediocritatem has ut. Denique constituto nec in. Hinc movet iriure eos ut, ut nec modus delenit, quo no quod corpora expetendis.
+                        Duo ei choro labores. Erat dolorem convenire ea sed. Qui wisi omnesque ad, per cu voluptatum intellegam.
                     </p>
                     <table className='table'>
                         <tbody>
@@ -88,6 +145,7 @@ var SideBar = React.createClass({
                             </tr>
                         </tbody>
                     </table>
+                    {radioButtons}
                     <button onClick={this.props.switchView} type='button' className='btn btn-danger btn-block'>Switch</button>
                 </div>
             </div>
@@ -96,16 +154,15 @@ var SideBar = React.createClass({
 });
 
 var mouseOverPolygon = function(map, event) {
-    console.log(event.feature.getProperty('NOMBRE'));
-    // map.data.revertStyle();
-    // map.data.overrideStyle(event.feature, {
-    //     strokeWeight: 1,
-    //     fillColor: '#ddd'
-    // });
+    map.data.revertStyle();
+    map.data.overrideStyle(event.feature, {
+        strokeWeight: 1,
+        fillColor: '#ddd'
+    });
 }
 
 var mouseOutPolygon = function(map, event) {
-    // map.data.revertStyle();
+    map.data.revertStyle();
 }
 
 var mouseClickPolygon = function(map, event) {
